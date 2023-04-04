6c79e778155df0c037a0cdadaa51e9c384380681
ASUS <ASUS@DESKTOP-TRT6E7B>
2023-04-04 21:52:23 +0200

    Ajout des fichiers manquants (__4)

--- src/main/java/mg/itu/randrianarivelo/jsf/Bean3.java
+++ src/main/java/mg/itu/randrianarivelo/jsf/Bean3.java
@@ -4,14 +4,16 @@
  */
 package mg.itu.randrianarivelo.jsf;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 
-@Named(value = "bean2")
+@Named(value = "bean3")
 @RequestScoped
-public class Bean2 {
+public class Bean3 {
 
     private int nombre;
 
@@ -33,6 +35,6 @@ public class Bean2 {
     }
     
     public String afficher() {
-        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
 }
